fde503691018ac58991f5e38c01c68f21e36cbac
Adam Ginzberg <ginzberg@stanford.edu>
2016-03-08 12:35:52 -0800

    new static finders

--- csp/settings.py
+++ csp/settings.py
@@ -42,8 +42,6 @@ STATIC_URL = '/static/'
 STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.FileSystemFinder',
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
-    # other finders..
-    'compressor.finders.CompressorFinder',
 )
 
 REST_FRAMEWORK = {
