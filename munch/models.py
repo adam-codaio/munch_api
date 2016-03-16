e2289066951d5d06af2130745589f5c7ed269878
Adam Ginzberg <ginzberg@stanford.edu>
2016-03-15 23:29:29 -0700

    remove phone number bullshit

--- munch/models.py
+++ munch/models.py
@@ -2,17 +2,11 @@ from __future__ import unicode_literals
 
 from django.db import models
 from django.contrib.auth.models import User
-from django.core.validators import RegexValidator
-
-class PhoneNumber(models.Model):
-    phone_regex = RegexValidator(regex=r'^\+?1?\d{9,15}$', 
-    				message="Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed.")
-    phone_number = models.CharField(max_length=17, validators=[phone_regex])
 
 class Restaurant(models.Model):
 	user = models.OneToOneField(User)
 	name = models.CharField(max_length=32)
-	phone_number = models.ForeignKey(PhoneNumber, default=None, null=True, blank=True, on_delete=models.CASCADE)
+	phone_number = models.CharField(max_length=20)
 	#Maybe regulate format of these a bit more in the future
 	address = models.CharField(max_length=128, blank=True)
 	hours = models.CharField(max_length=256, blank=True)
--- munch/serializers/restaurant.py
+++ munch/serializers/restaurant.py
@@ -2,12 +2,6 @@ from munch import models
 from rest_framework import serializers
 from munch.serializers.dynamic import DynamicFieldsModelSerializer
 
-
-class PhoneNumberSerializer(serializers.ModelSerializer):
-	class Meta:
-		model = models.PhoneNumber
-		fields = ('id', 'phone_number',)
-
 class RestaurantSerializer(DynamicFieldsModelSerializer):
     class Meta:
         model = models.Restaurant
